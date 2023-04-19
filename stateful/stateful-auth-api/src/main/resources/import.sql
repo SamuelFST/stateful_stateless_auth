c3944951a71bc1816499a18ca56371ec4853d74c
SamuelFST <guti.samuel2002@gmail.com>
2023-04-18 22:32:36 -0300

    connection to db, User model and initial db data

---
+++ stateful/stateful-auth-api/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, username, password) VALUES (1, 'admin', '$2a$08$QtD1j.y/qPpmNOyw4WD79.Y8xAm27lx.v8GOdvbdH8kJjCBrNRThu');
+INSERT INTO users (id, username, password) VALUES (2, 'newadmin', '$2a$08$QtD1j.y/qPpmNOyw4WD79.Y8xAm27lx.v8GOdvbdH8kJjCBrNRThu');
---
+++ stateless/stateless-auth-api/src/main/java/br/com/microservice/statelessauthapi/core/model/User.java
@@ -0,0 +1,56 @@
+package br.com.microservice.statelessauthapi.core.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users", schema = "public")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "username", nullable = false, unique = true)
+	private String username;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	public User() {
+	}
+
+	public User(Integer id, String username, String password) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
