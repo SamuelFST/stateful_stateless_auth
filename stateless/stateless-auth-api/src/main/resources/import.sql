8e03761b39b936e66eaa1ac580cd5750b33c8e1a
SamuelFST <guti.samuel2002@gmail.com>
2023-11-01 13:29:26 -0300

    update ids of import.sql inserts

--- stateless/stateless-auth-api/src/main/resources/import.sql
+++ stateless/stateless-auth-api/src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (id, username, password) VALUES (1, 'admin', '$2a$08$QtD1j.y/qPpmNOyw4WD79.Y8xAm27lx.v8GOdvbdH8kJjCBrNRThu');
-INSERT INTO users (id, username, password) VALUES (2, 'newadmin', '$2a$08$QtD1j.y/qPpmNOyw4WD79.Y8xAm27lx.v8GOdvbdH8kJjCBrNRThu');
+INSERT INTO users (id, username, password) VALUES (1000, 'admin', '$2a$08$QtD1j.y/qPpmNOyw4WD79.Y8xAm27lx.v8GOdvbdH8kJjCBrNRThu');
+INSERT INTO users (id, username, password) VALUES (2000, 'newadmin', '$2a$08$QtD1j.y/qPpmNOyw4WD79.Y8xAm27lx.v8GOdvbdH8kJjCBrNRThu');
