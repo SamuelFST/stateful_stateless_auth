e41357d3ddc1a1dbd1dbb107ae72c0ea517f3c15
SamuelFST <guti.samuel2002@gmail.com>
2023-11-01 13:32:48 -0300

    user repository created

---
+++ stateless/stateless-auth-api/src/main/java/br/com/microservice/statelessauthapi/core/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package br.com.microservice.statelessauthapi.core.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.microservice.statelessauthapi.core.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+	Optional<User> findByUsername(String username);
+}
