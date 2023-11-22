6e70f9957554153cfc2019c839ff7ac1eb4f0bfa
SamuelFST <guti.samuel2002@gmail.com>
2023-11-22 20:05:09 -0300

    password-encoder and redis config, user not found exception created in stateful auth-api

--- stateful/stateful-auth-api/src/main/java/br/com/microservice/statefulauthapi/infra/exception/ExceptionGlobalHandler.java
+++ stateful/stateful-auth-api/src/main/java/br/com/microservice/statefulauthapi/infra/exception/ExceptionGlobalHandler.java
@@ -10,13 +10,22 @@ public class ExceptionGlobalHandler {
 
 	@ExceptionHandler(ValidationException.class)
 	public ResponseEntity<?> handleValidationException(ValidationException validationException) {
-		ExceptionDetails details = new ExceptionDetails(HttpStatus.BAD_REQUEST.value(), validationException.getMessage());
+		ExceptionDetails details = new ExceptionDetails(HttpStatus.BAD_REQUEST.value(),
+				validationException.getMessage());
 		return new ResponseEntity<>(details, HttpStatus.BAD_REQUEST);
 	}
-	
+
 	@ExceptionHandler(AuthenticationException.class)
 	public ResponseEntity<?> handleAuthenticationException(AuthenticationException authenticationException) {
-		ExceptionDetails details = new ExceptionDetails(HttpStatus.UNAUTHORIZED.value(), authenticationException.getMessage());
+		ExceptionDetails details = new ExceptionDetails(HttpStatus.UNAUTHORIZED.value(),
+				authenticationException.getMessage());
 		return new ResponseEntity<>(details, HttpStatus.UNAUTHORIZED);
+	}
+
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException userNotFoundException) {
+		ExceptionDetails details = new ExceptionDetails(HttpStatus.NOT_FOUND.value(),
+				userNotFoundException.getMessage());
+		return new ResponseEntity<>(details, HttpStatus.NOT_FOUND);
 	}
 }
