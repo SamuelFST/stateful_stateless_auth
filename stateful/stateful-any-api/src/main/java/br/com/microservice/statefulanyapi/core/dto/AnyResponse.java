fe180ef2274d3dd04632c9f19b48a219b9f2655d
SamuelFST <guti.samuel2002@gmail.com>
2023-11-30 22:18:15 -0300

    any-response, auth-user-response and tokenDto dtos added, token client created on stateful any-api

---
+++ stateful/stateful-any-api/src/main/java/br/com/microservice/statefulanyapi/core/dto/AnyResponse.java
@@ -0,0 +1,5 @@
+package br.com.microservice.statefulanyapi.core.dto;
+
+public record AnyResponse(String status, Integer code, AuthUserResponse authUser) {
+
+}
---
+++ stateful/stateful-any-api/src/main/java/br/com/microservice/statefulanyapi/core/dto/TokenDTO.java
@@ -0,0 +1,5 @@
+package br.com.microservice.statefulanyapi.core.dto;
+
+public record TokenDTO(String accessToken) {
+
+}
